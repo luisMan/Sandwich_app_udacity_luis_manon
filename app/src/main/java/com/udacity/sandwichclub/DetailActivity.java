e168755769b0528d136e972bacd2d97f13f68e37
luis <luisman1989@gmail.com>
2018-05-08 22:40:36 -0400

    Well google map api feature has been accomplish
    my first nano degree google udacity project sandwich app is done and
    waiting for my mentors feedback

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -1,6 +1,8 @@
 package com.udacity.sandwichclub;
 
 import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,15 +12,28 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
-public class DetailActivity extends AppCompatActivity {
+import java.util.List;
+
+public class DetailActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
     private static Sandwich sandwich = null;
+    //this is our mapview component for the origin data
+    private MapView mapView;
+    //our google map object
+    private GoogleMap map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +49,21 @@ public class DetailActivity extends AppCompatActivity {
             closeOnError();
             logOut("intent is a null value");
         }
+
+        //lets get the map object and instantiate our null mapView
+        if (findViewById(R.id.map) != null) {
+
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            mapView = (MapView) findViewById(R.id.map);
+            mapView.onCreate(savedInstanceState);
+            mapView.onResume();
+            mapView.getMapAsync(this);
+
+        }//end of mapview
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
         if (position == DEFAULT_POSITION) {
@@ -53,7 +83,6 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
 
         //wao didn't know about this library picaso This class is so fun!
         Picasso.with(this)
@@ -61,6 +90,8 @@ public class DetailActivity extends AppCompatActivity {
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
+
+
     }
 
     private void closeOnError() {
@@ -68,6 +99,21 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
+    //lets get lat and long of giving string location
+    public Address getLoactionLatLong(String params)
+    {
+        Geocoder gc =  new Geocoder(getApplicationContext());
+        Address location =null;
+
+        try {
+            List<Address> list = gc.getFromLocationName(params, 1);
+            location = list.get(0);
+            Toast.makeText(getApplicationContext(),"the origin : "+location.getLocality(),Toast.LENGTH_LONG).show();
+
+        }catch (Exception e){e.printStackTrace();}
+
+    return location;
+    }
 
     private void populateUI() {
         Animation fadeIn = new AlphaAnimation(0, 1);
@@ -87,10 +133,29 @@ public class DetailActivity extends AppCompatActivity {
         //lets populate the data
         if(sandwich!=null)
         {
-
-            alsoKnownAs.setText(sandwich.getAlsoKnownAs().toString());
-            ingredients.setText(sandwich.getIngredients().toString());
             placeOfOrigin.setText(sandwich.getPlaceOfOrigin().toString());
+            //lets point the map to the correct origin address location
+            if(placeOfOrigin.getText().length() > 0 && !placeOfOrigin.getText().equals("Unknown"))
+            {
+                Address origin = getLoactionLatLong(placeOfOrigin.getText().toString());
+                //now lets move map viewport to current place location
+                if(origin!=null)
+                MoveMapViewPort(sandwich.getMainName(),origin);
+
+            }
+
+            for(String s : sandwich.getAlsoKnownAs())
+            {
+              alsoKnownAs.append("\n* "+s.toString());
+            }
+
+
+            for(String s : sandwich.getIngredients())
+            {
+                ingredients.append("\n* "+s.toString());
+            }
+
+
             description.setText(sandwich.getDescription().toString());
         }
 
@@ -103,5 +168,20 @@ public class DetailActivity extends AppCompatActivity {
     private void logOut(String text)
     {
         Log.d(DetailActivity.class.getName(),text);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        populateUI();
+    }
+
+    public void MoveMapViewPort(String sandwich_name,Address location)
+    {
+       // Toast.makeText(getApplicationContext(),location.toString(),Toast.LENGTH_LONG).show();
+            LatLng latlong = new LatLng(location.getLatitude(),location.getLongitude());
+            map.addMarker(new MarkerOptions().position(latlong).title(sandwich_name));
+            map.moveCamera(CameraUpdateFactory.newLatLng(latlong));
+
     }
 }
